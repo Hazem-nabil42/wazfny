8c6d41ef7dd1da13f8dbab628d9beaf52e80a0c8
unknown <hazem-nabil42>
2024-03-15 19:47:59 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/index.html'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        gadm: ['El Messiri'] 
+      },
+      backgroundImage: {
+        "hero": "url('https://img.freepik.com/premium-photo/black-wallpaper-that-says-black-wallpapers-black-wallpaper-black-wallpaper-black-wallpaper-black-wallpaper-black-wallpaper-black-wallpaper-black-wallpaper-black_916626-1681.jpg')",
+        "SEO": "url('https://assets.entrepreneur.com/content/3x2/2000/1648113590-new4.jpg')",
+      },
+      keyframes: {
+        scale: {
+          '0%, 100%': { filter: 'brightness(1.5);' },
+          '50%': { filter: 'brightness(0.25);' },
+        }
+      },
+      animation: {
+        scale: 'scale 1s ease-in-out infinite',
+      }
+    },
+  },
+  plugins: [],
+}
+
